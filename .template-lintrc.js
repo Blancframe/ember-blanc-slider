ca5c1960a35bbfa67c9278b8be57e9463078e69d
JhonM <jhonmajoor@gmail.com>
2020-07-27 10:48:10 +0200

    Add template lint prettier (not working)

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  extends: ['recommend', 'ember-template-lint-plugin-prettier:recommended'],
+  plugins: ['ember-template-lint-plugin-prettier'],
   rules: {
-    'bare-strings': true,
+    'bare-strings': false,
     'block-indentation': 4,
     'nested-interactive': true,
     'self-closing-void-elements': 'require',
