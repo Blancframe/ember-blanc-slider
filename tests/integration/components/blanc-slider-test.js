e77f84e71e40b3b07f4f3e1c74eceb5b25ac202f
JhonM <jhonmajoor@gmail.com>
2020-07-29 16:04:45 +0200

    Make tests pass

--- tests/integration/components/blanc-slider-test.js
+++ tests/integration/components/blanc-slider-test.js
@@ -2,18 +2,12 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { fullTrim } from 'dummy/tests/helpers/string-helpers';
 
 module('Integration | Component | blanc-slider', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{blanc-slider}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
     // Template block usage:
     await render(hbs`
       {{#blanc-slider}}
@@ -21,6 +15,9 @@ module('Integration | Component | blanc-slider', function (hooks) {
       {{/blanc-slider}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(
+      fullTrim(this.element.textContent),
+      'template block text Previous Next'
+    );
   });
 });
