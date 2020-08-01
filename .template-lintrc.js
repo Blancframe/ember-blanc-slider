9277dc4a44004e0c393b32ea925f1a2a93506df6
JhonM <jhonmajoor@gmail.com>
2020-08-01 22:27:52 +0200

    Accept bare strings

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   extends: 'recommended',
   rules: {
-    'no-bare-strings': true,
+    'no-bare-strings': false,
     'block-indentation': 4,
     'nested-interactive': true,
     'self-closing-void-elements': 'require',
