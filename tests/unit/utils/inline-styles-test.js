c0377cfa074c2428f66d855361a206147410e534
JhonM <jhonmajoor@gmail.com>
2020-08-05 20:03:25 +0200

    Make sure to run linter locally

--- tests/unit/utils/inline-styles-test.js
+++ tests/unit/utils/inline-styles-test.js
@@ -1,7 +1,7 @@
 import inlineStyles from 'dummy/utils/inline-styles';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | inline-styles', function (hooks) {
+module('Unit | Utility | inline-styles', function () {
   // Replace this with your real tests.
   test('it works', function (assert) {
     let result = inlineStyles();
